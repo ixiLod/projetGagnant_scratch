a44465875c4e9103258c063b5f76e11900c8a465
Dolix7 <51421090+Dolix7@users.noreply.github.com>
2022-02-28 17:00:45 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const allBtn = document.querySelectorAll('.product-hamburger');
+const navMenu = document.querySelector('.naav');
+
+allBtn.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        btn.classList.toggle('active');
+        navMenu.classList.toggle('active-menu');
+    });
+});
+
+
+
